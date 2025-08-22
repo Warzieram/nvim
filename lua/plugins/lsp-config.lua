0728297f8149cf1686c16c9ca1fab8f98ecefbc2
RAMASSAMY Luc <luko974@gmail.com>
2025-08-22 15:07:14 +0200

    added dart support to lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -44,6 +44,7 @@ return {
       lspconfig.gopls.setup({ capabilities = capabilities })
       lspconfig.jdtls.setup({ capabilities = capabilities })
       lspconfig.angularls.setup({ capabilities = capabilities })
+      lspconfig.dartls.setup({ capabilities = capabilities })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true, silent = true })
